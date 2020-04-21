e59c3c668bbea59f4b8cbd88833df398f30ef5b7
linux_china <libing.chen@gmail.com>
2020-04-21 12:34:28 -0700

    polishing

--- code/equals.ts
+++ code/equals.ts
@@ -1,6 +1,6 @@
 //Equals Operator ( == ), Strict Equals Operator ( === )
 let a: any = 10;
 let b: any = "10"
-console.log(a == b);   //true
-console.log(a == b); //true
-console.log(a === b); //false
+console.log(a == b);  //true
+console.log(a == b);  //true
+console.log(a === b); //false, type different
