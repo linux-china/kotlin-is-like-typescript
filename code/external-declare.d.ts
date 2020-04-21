2a9a7a129c477840c112a42f398f4418465ea62d
linux_china <libing.chen@gmail.com>
2020-04-21 15:03:23 -0700

    external declare

---
+++ code/external-declare.d.ts
@@ -0,0 +1,9 @@
+//index.d.ts
+export = _;
+export as namespace _;
+
+declare const _: _.LoDashStatic;
+declare namespace _ {
+    interface LoDashStatic {
+    }
+}
---
+++ code/external-declare.kt
@@ -0,0 +1,5 @@
+@file:JsModule("lodash")
+package lodash
+
+@JsName("toUpper")
+external fun toUpper(text: String): String
