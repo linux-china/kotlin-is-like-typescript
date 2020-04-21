53a77ca8b3d6e0372f87286e2823ec3810fb802e
linux_china <libing.chen@gmail.com>
2020-04-21 11:29:51 -0700

    add more types

--- code/explicit-types.kt
+++ code/explicit-types.kt
@@ -1,1 +1,6 @@
 val explicitDouble: Double = 70.0
+// Boolean
+// Byte, Short, Int, Long, Float, Double, UByte, UShort, UInt, ULong
+// String
+// Any, Unit
+// Nothing
--- code/explicit-types.ts
+++ code/explicit-types.ts
@@ -1,1 +1,7 @@
 const explicitDouble: number = 70;
+// boolean
+// number
+// string
+// any, void
+// never
+// undefined, null, union: string|number, string|object
