4f8c227b628fc768401588eb16839b42792e9b63
linux_china <libing.chen@gmail.com>
2020-04-21 11:51:27 -0700

    add listOf

--- code/arrays.kt
+++ code/arrays.kt
@@ -1,3 +1,5 @@
 val shoppingList = arrayOf("catfish", "water",
     "tulips", "blue paint")
 shoppingList[1] = "bottle of water"
+val freezeList = listOf("first", "second", "third")
+val mutableList = mutableListOf("first", "second", "third")
