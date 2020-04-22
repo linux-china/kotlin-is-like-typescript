2f93dcd874bb33d98fe0847e0ce7be899d39d323
linux_china <libing.chen@gmail.com>
2020-04-22 14:37:10 -0700

    data class

---
+++ code/data-class.kt
@@ -0,0 +1,3 @@
+data class Point(val x: Double, val y: Double)
+
+val point = Point(1.0, 2.0);
---
+++ code/data-class.ts
@@ -0,0 +1,6 @@
+type Point = {
+    x: number,
+    y: number
+}
+
+let point: Point = {x: 1.0, y: 2.0};
