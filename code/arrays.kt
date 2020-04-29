ea59358b2b7a0f681679963c64c6779930754ad0
linux_china <libing.chen@gmail.com>
2020-04-28 23:06:24 -0700

    polishing

--- code/arrays.kt
+++ code/arrays.kt
@@ -1,5 +1,9 @@
-val shoppingList = arrayOf("catfish", "water",
-    "tulips", "blue paint")
-shoppingList[1] = "bottle of water"
-val freezeList = listOf("first", "second", "third")
-val mutableList = mutableListOf("first", "second", "third")
+fun main12() {
+    val shoppingList = arrayOf(
+        "catfish", "water",
+        "tulips", "blue paint"
+    )
+    shoppingList[1] = "bottle of water"
+    val freezeList = listOf("first", "second", "third")
+    val mutableList = mutableListOf("first", "second", "third")
+}
