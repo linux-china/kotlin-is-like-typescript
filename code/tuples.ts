8b007830ac3a78c16f5c41236b114b75b7760b3b
linux_china <libing.chen@gmail.com>
2020-08-18 11:58:00 -0700

    add Labeled Tuple

--- code/tuples.ts
+++ code/tuples.ts
@@ -1,5 +1,8 @@
 type GasPrices = [number, number, number]
 
+//Labeled Tuple
+type Range = [start: number, end: number];
+
 function main4() {
     let price: GasPrices = [3.59, 3.69, 3.79]
     let [a, b, c] = price;
