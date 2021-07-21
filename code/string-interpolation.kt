45a0ebf711104c54d11965cdbf3627421b413eba
linux_china <libing.chen@gmail.com>
2021-07-20 23:39:06 -0700

    Add more examples

--- code/string-interpolation.kt
+++ code/string-interpolation.kt
@@ -1,4 +1,5 @@
 val apples = 3
 val oranges = 5
-val fruitSummary = "I have ${apples + oranges} " +
-                   "pieces of fruit."
+val fruitSummary1 = "I have ${apples + oranges} pieces of fruit."
+val fruitSummary2 = """I have ${apples + oranges} pieces of fruit."""
+val fruitSummary3 = html("""I have ${apples + oranges} pieces of fruit.""")
