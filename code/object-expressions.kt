cb90b14067e99bd4df9dbef5e97b1587d736a8e2
linux_china <libing.chen@gmail.com>
2021-07-21 17:44:10 -0700

    Object expression

---
+++ code/object-expressions.kt
@@ -0,0 +1,6 @@
+val helloWorld = object {
+    val hello = "Hello"
+    val world = "World"
+    // object expressions extend Any, so `override` is required on `toString()`
+    override fun toString() = "$hello $world"
+}
